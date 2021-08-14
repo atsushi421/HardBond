3b3d2a85f00840a0f8dd4ee70c6126c878ed22a6
yunicode <tkmpan2021@gmail.com>
2021-08-15 08:27:53 +0900

    戦闘画面の値受け渡し

--- application/views/battle.py
+++ application/views/battle.py
@@ -3,20 +3,26 @@ from django.shortcuts import render
 from ..models import Obon
 from ..models import Result
 from django.contrib.auth import get_user_model
+import json
+import math
 
 User = get_user_model()
 
 class BattleView(View):
     def get(self, request, *args, **kwargs):
         obon = Obon.objects.get(user_id=request.user.id)
-        
+
+        # 時間を計算
+        time =  math.floor((obon.size + obon.wise + obon.weight + obon.motivation) / 12)
+
         # 飛距離を計算
-        distance = obon.size + obon.wise + obon.weight + obon.motivation
+        distance = time * 90 
         result = Result.objects.create(name=obon.name, result=distance)
         result.save()
         
-        context = {
-            'obon' : obon
+        data = {
+            "imageUrl" : obon.image.url,
+            "time" : time,
         }
         
-        return render(request, 'battle.html', context)
+        return render(request, 'battle.html', {'data_json': json.dumps(data)})
--- static/flying.js
+++ static/flying.js
@@ -2,14 +2,22 @@ let daytime = document.querySelector(".daytime");
 let obon = document.querySelector(".obon");
 
 let startTime = 0;
-const flyingTime = 5;
+
+let json = ""
+
+
+// const flyingTime = json.time;
+let flyingTime = 5;
 const endDistance = 90 * flyingTime;
 
+
+
 addEventListener('keypress', function(e){
-    console.log(e)
     if(e.key == "Enter"){
         // daytime.classList.toggle("moving");
         // obon.classList.toggle("down");
+        console.log(json.time);
+        flyingTime = json.time;
         document.getElementById("daytime").style.animation = "moving " + flyingTime + "s linear";
         document.getElementById("obon").style.animation = "down " + flyingTime + "s linear";
         document.getElementById("obon").getElementsByTagName("img")[0].classList.add("rotate");
@@ -17,8 +25,9 @@ addEventListener('keypress', function(e){
         
         // 一定時間ごとに距離を計算
         const intervalId = setInterval(() =>{
-            let dis = calcDistance();
-            if(dis >= endDistance){　
+            let duaration = Date.now() - startTime;
+            let dis = calcDistance(duaration);
+            if(duaration / 1000  >= flyingTime){
                 clearInterval(intervalId);　//intervalIdをclearIntervalで指定している
                 endView(dis - (( 90.0 / 1000 ) * 500));
             }
@@ -28,16 +37,15 @@ addEventListener('keypress', function(e){
 
 
 let dis = document.getElementById("distance");
-function calcDistance(){
-    let now = Date.now();
-    console.log(now - startTime);
-    var distance = ( 90 / 1000 ) * (now - startTime);
+function calcDistance(duaration){
+    var distance = ( 90 / 1000 ) * duaration;
     dis.textContent = Math.floor(distance) + " km";
-    return distance
+    return Math.floor(distance)
 }
 
 function endView(dis){
-    // window.Location.href = '/home';
     window.alert("あなたの記録は" + Math.floor(dis) + "kmです！");
+    console.log(dis);
+    console.log(endDistance);
     location.replace('/results/');
 }
